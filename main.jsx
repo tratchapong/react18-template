7c685201dc3cc5902fc86b5132827d01e6498b22
tratchapong <tratchapong@gmail.com>
2025-05-09 23:13:21 +0700

    Add tailwind4

--- main.jsx
+++ main.jsx
@@ -1,13 +1,11 @@
 function App() {
-
-  
   return (
-    <div className="app">
-      <h1>React18 Playground</h1>
+    <div className="app bg-pink-200 p-4">
+      <h1 className='text-4xl text-center text-blue-400'>React18 Playground</h1>
+      <p className='text-xl text-violet-400 text-center my-3'>Codecamp Thailand</p>
     </div>
   )
 }
-
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(<App />)
